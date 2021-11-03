030d10ad182642e7fa1d79c2a3a474e02ef1d9cb
Gid Baerridge <gid.berridge@gmail.com>
2021-11-03 18:10:42 +0000

    updated profile

--- src/data.js
+++ src/data.js
@@ -1,35 +1,27 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Vinyltracer",
+    subtitle: "Ruby on Rails, Bootstrap, CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "A music suggestion app that utilises web scraping and access API's for its data",
+    image: "./vinyltracer_screenshot.jpg",
+    link: "https://vinyltracer.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Refuge",
+    subtitle: "HTML / CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "A static website made for a band using HTML / CSS",
+    image: "./refuge_screenshot.jpg",
+    link: "https://officialrefuge.com",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Spanner HQ",
+    subtitle: "Wordpress / CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "A Wordpress site made for a musician using Worpress as a CMS and is currently updated by the artist. ",
+    image: "./spanner_screenshot.jpg",
+    link: "https://spannerhq.com",
   },
 ];
 
@@ -53,8 +45,10 @@ export const testimonials = [
 export const skills = [
   "JavaScript",
   "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+  "Ruby on Rails",
+  "HTML",
+  "CSS",
+  "Python",
+  "Bootstrap",
+  "Tailwind",
 ];
