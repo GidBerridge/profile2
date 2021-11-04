928d3b43067e4963c692186b3e799e08f4916484
Gid Baerridge <gid.berridge@gmail.com>
2021-11-04 18:10:07 +0000

    all

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col place-self-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-white">
             Gid Berridge
@@ -26,13 +26,13 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"> 
-          {/* <img
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"> 
+          <img
             className="object-cover object-center rounded"
             alt="hero"
             src="./coding.svg"
-          /> */}
-        </div>
+          />
+        </div> */}
       </div>
     </section>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,12 +25,12 @@ export default function Contact() {
       }
 
   return (
-    <section id="contact" className="relative">
-      <div className="container bg-gray-900 px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-      <div className="relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+    <section id="contact" className="relative bg-gray-900">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="relative flex flex-wrap items-center py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0 ">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL   
+                EMAIL    
               </h2>
               <a className="text-indigo-400 leading-relaxed">
                 gid.berridge@gmail.com
@@ -70,7 +70,7 @@ export default function Contact() {
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
-          <p className="leading-relaxed mb-5">
+          <p className="leading-relaxed mb-5 text-white">
             Need a website? Help with a freelance project? short term, long term or permanent position filled? Let me know...
           </p>
           <div className="relative mb-4">
