8b177bb00b41b1fde56df03ecb8072d4e282017f
Gid Baerridge <gid.berridge@gmail.com>
2021-11-04 16:16:07 +0000

    background changed

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,10 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white titles">
-            Hi, I'm Gid.
-            {/* <br className="hidden lg:inline-block" />I love to build amazing
-            apps. */}
+          <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-white">
+            Gid Berridge
           </h1>
+          <h2 className="text-white mb-1 font-medium title-font">Full Stack Developer</h2>
           <p className="mb-8 leading-relaxed">
             I'm a full stack developer based in London (UK) and happy to work locally or remotely. I've worked on a number
             of front-end, back-end and no-code projects. 
@@ -28,11 +27,11 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"> 
-          <img
+          {/* <img
             className="object-cover object-center rounded"
             alt="hero"
             src="./coding.svg"
-          />
+          /> */}
         </div>
       </div>
     </section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,16 +27,7 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-      <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            {/* <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
-            </div> */}
+      <div className="relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL   
@@ -58,7 +49,7 @@ export default function Contact() {
               <p className="text-indigo-400 leading-relaxed"><a href="https://www.linkedin.com/in/gideon-berridge/">https://www.linkedin.com/in/gideon-berridge/</a></p>
             </div>
           </div>
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
@@ -71,7 +62,7 @@ export default function Contact() {
             src="https://www.google.com/maps/embed/v1/place?q=Turnpike+Lane&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           
-        </div>
+        </div> */}
         <form
           netlify
           name="contact"
