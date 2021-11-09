f873f77c0646a3600451c3d3564b316bfd8fedad
Gid Baerridge <gid.berridge@gmail.com>
2021-11-09 09:20:22 +0000

    hire me form deleted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,7 @@ export default function Contact() {
           />
           
         </div> */}
-        <form
+        {/* <form
           netlify
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -112,7 +112,7 @@ export default function Contact() {
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
-        </form>
+        </form> */}
       </div>
     </section>
   );
