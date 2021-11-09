d7b2063e997a5925fb52c2d1ae27814a1a46e41e
Gid Baerridge <gid.berridge@gmail.com>
2021-11-09 20:43:55 +0000

    responsiveness adjusted

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col place-self-center">
-        <div className="md:flex-grow md:w-1/2 flex flex-col  mb-16 md:mb-0 items-center text-center">
+        <div className="md:flex-grow md:w-1/2 flex flex-col mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-white text-center">
             Gideon Berridge
           </h1>
