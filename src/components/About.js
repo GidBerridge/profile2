d598ea45f19e958e5d34413aa6c77ab35ffde216
Gid Baerridge <gid.berridge@gmail.com>
2021-11-14 12:39:59 +0000

    all

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col place-self-center">
         <div className="md:flex-grow md:w-1/2 flex flex-col mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-white text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-black text-center">
             Gideon Berridge 
           </h1>
           <h2 className="text-white mb-4 font-medium title-font">Full Stack Developer</h2>
@@ -25,61 +25,61 @@ export default function About() {
 }
 
 
-const TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 5000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
+// const TxtRotate = function(el, toRotate, period) {
+//   this.toRotate = toRotate;
+//   this.el = el;
+//   this.loopNum = 0;
+//   this.period = parseInt(period, 10) || 5000;
+//   this.txt = '';
+//   this.tick();
+//   this.isDeleting = false;
+// };
 
-TxtRotate.prototype.tick = function() {
-  const i = this.loopNum % this.toRotate.length;
-  const fullTxt = this.toRotate[i];
+// TxtRotate.prototype.tick = function() {
+//   const i = this.loopNum % this.toRotate.length;
+//   const fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+//   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-  const that = this;
-  let delta = 100 - Math.random() * 100;
+//   const that = this;
+//   let delta = 100 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+//   if (this.isDeleting) { delta /= 2; }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     delta = this.period;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === '') {
+//     this.isDeleting = false;
+//     this.loopNum++;
+//     delta = 500;
+//   }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+//   setTimeout(function() {
+//     that.tick();
+//   }, delta);
+// };
 
-window.onload = function() {
-  const elements = document.getElementsByClassName('txt-rotate');
-  for (let i=0; i<elements.length; i++) {
-    const toRotate = elements[i].getAttribute('data-rotate');
-    const period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  const css = document.createElement("style");
-  // css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+// window.onload = function() {
+//   const elements = document.getElementsByClassName('txt-rotate');
+//   for (let i=0; i<elements.length; i++) {
+//     const toRotate = elements[i].getAttribute('data-rotate');
+//     const period = elements[i].getAttribute('data-period');
+//     if (toRotate) {
+//       new TxtRotate(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   const css = document.createElement("style");
+//   // css.type = "text/css";
+//   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+//   document.body.appendChild(css);
+// };
   
 
