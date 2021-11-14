ad69754b995e6b2a5a95523fb0d5cfc1f2e6c6c2
Gid Baerridge <gid.berridge@gmail.com>
2021-11-14 18:13:35 +0000

    projects minoe changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,3 @@
-
-import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
 
@@ -20,17 +18,17 @@ export default function Projects() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2">
-              <div className="flex relative">
+              className="">
+              <div className="">
                 <img
                   alt="gallery"
                   className="projects-card"
                   src={project.image}
                 />
           
-                <div className="px-8 py-10 relative z-10 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
+                <div className="z-10 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                      {project.title}
                   </h1>
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
