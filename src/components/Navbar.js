ac32cdfec017bb6ef9ba80d5507bf73ffcf7478f
Gid Baerridge <gid.berridge@gmail.com>
2021-11-14 17:43:26 +0000

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,23 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl hover:text-yellow-500">
+        
+        <nav className="md:mr-auto md:ml-auto md:py-1 md:pl-4 	flex flex-wrap items-center text-base justify-center">
+          <a href="#about" className="mr-6 text-white hover:text-yellow-500">
             About
           </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 text-white hover:text-yellow-500">
+          <a href="#projects" className="mr-6 text-white hover:text-yellow-500">
             Past Work
           </a>
-          <a href="#skills" className="mr-5 text-white hover:text-yellow-500">
+          <a href="#skills" className="mr-6 text-white hover:text-yellow-500">
             Skills
           </a>
-          {/* <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
-          </a> */}
+          <a
+            href="#contact"
+            className="mr-5 text-white hover:text-yellow-500">
+            Contact
+          </a>
         </nav>
-        <a
-          href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:text-yellow-500 rounded text-white text-base mt-4 md:mt-0">
-          Contact
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
       </div>
     </header>
   );
